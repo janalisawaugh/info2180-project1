a07cfa62f90d7cab9561a429f770e62c0f1e2d49
Janalisa Waugh <iamjanalisa@gmail.com>
2022-10-30 23:42:21 -0500

    Completed javacript file to add functionality to subscribe button and completed styling in css file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 /* Add your JavaScript to this file */
+onload= function(){
+
+    var email = document.querySelector('#email');
+    var message = document.querySelector('.message');
+
+
+    addEventListener('submit', function (element){
+        element.preventDefault();               
+
+        if(!email.value==''){
+            message.innerHTML = `Thank you! Your email address ${email.value} has been added to our mailing list!`;
+            email.value = '';            
+        }
+        else{
+            
+            message.innerHTML = 'Please enter a valid email address';
+            email.value = '';
+        }
+    })
+
+
+};
